9f50594da5ced2e7d3bd4830932b657638f64bb8
Ariana <ariaguirre64@gmail.com>
2023-07-04 11:25:47 -0300

    01

--- 01_FirstPage/practice.php
+++ 01_FirstPage/practice.php
@@ -4,6 +4,10 @@
 		<title>My First PHP Web Page</title>
 	</head>
 	<body>
-		<?php print('Hola Mundo!'); ?>
+		<?php 
+		print('Hola Mundo! <br/>'); 
+		$color='rojo';
+		echo "Mi remera es de color " . $color . "<br/>";
+		?>
 	</body>
 </html>
