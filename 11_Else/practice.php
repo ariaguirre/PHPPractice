6c292e8d102d25ffa7e6fb17dcbddc8a556ccdd2
Ariana <ariaguirre64@gmail.com>
2023-07-04 15:51:08 -0300

    11

--- 11_Else/practice.php
+++ 11_Else/practice.php
@@ -1,6 +1,7 @@
 <?php
 	
 	// Constants
+	define('TITLE', 'Else Statement')
 
 	
 	// Custom Variables
@@ -11,7 +12,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,13 +21,20 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial 11: <small><?php echo TITLE?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
+			<?php 
+				$age = 20;
+			if($age > 18){
+				echo "Tenes $age? Sos mayor de edad.";
+			} else {
+				echo "Tenes $age? Sos menor de edad.";
+			}
+			?>
 			</div><!-- end sandbox -->
 			
 			<a href="index.php" class="button">Back to the lecture</a>
