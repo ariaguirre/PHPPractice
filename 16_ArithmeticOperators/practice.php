3cc8eb7fa129f995c927cb09ec4c78cf282f795a
Ariana <ariaguirre64@gmail.com>
2023-07-04 16:31:58 -0300

    16

--- 16_ArithmeticOperators/practice.php
+++ 16_ArithmeticOperators/practice.php
@@ -1,17 +1,13 @@
 <?php
-	
-	// Constants
-
-	
-	// Custom Variables
-
+	define("TITLE", "Arithmetic Operators");
+	$lessonNum	= 16;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP - <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,36 +16,55 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
-				
-				<h3>Addition <code>+</code></h3>
+			<h3>Addition <code>+</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+					$b = 19;
+					$c = ($a + $b);
+					
+					echo "$a + $b is equal to $c";
 				?>
 				
 				<h3>Subtraction <code>-</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+					$b = 19;
+					$c = ($a - $b);
+					
+					echo "$a - $b is equal to $c";
 				?>
 				
 				<h3>Multiplication <code>*</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+					$b = 19;
+					$c = ($a * $b);
+					
+					echo "$a &times; $b is equal to $c";
 				?>
 				
 				<h3>Division <code>/</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+					$b = 19;
+					$c = ($a / $b);
+					
+					echo "$a / $b is equal to $c";
 				?>
 				
 				<h3>Modulus <code>%</code></h3>
 				<?php
-					// your code here
+					$a = 7;
+					$b = 19;
+					$c = ($a % $b);
+					
+					echo "$a % $b is equal to $c";
 				?>
 				
 			</div><!-- end sandbox -->
