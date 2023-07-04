f0fdb38f2649960d9d0394437610e9baaae169d3
Ariana <ariaguirre64@gmail.com>
2023-07-04 12:43:22 -0300

    07

--- 07_AssociativeArrays/practice.php
+++ 07_AssociativeArrays/practice.php
@@ -1,19 +1,25 @@
 <?php
 	
 	// Constants
-	
+	define("TITLE", "Associative Arrays");
+
 	
 	// Custom Variables
 
 	
-	// Moustache Associative Array
-
+	// User Associative Array
+	$user = array(
+		"name" => "Maria",
+		"email"=> "maria@gmail.com",
+		"orders" => "5"
+	)
+	
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- PAGE TITLE --></title>
+		<title>PHP <?php echo TITLE?></title>
 		<link href="/assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -22,15 +28,15 @@
 				<img src="/assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- PAGE TITLE --></small></h1>
+			<h1>Tutorial 07: <small><?php echo TITLE?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
 			
-				<h2>The <!-- MOUSTACHE NAME --> Moustache!</h2>
-				<p>This moustache is quite the dirt squirrel! It boasts a creep factor of <strong><!-- CREEP FACTOR --></strong> and takes <strong><!-- GROWTH DAYS --> days</strong> to grow on average.</strong></p>
+				<h2>The user <?php echo $user["name"]?>:</h2>
+				<p>This user is registered under the email <strong> <?php echo $user["email"]?> </strong> and has made <strong><?php echo $user["orders"]?> </strong> orders in the last month.</p>
 				
 			</div><!-- end sandbox -->
 			
@@ -38,7 +44,7 @@
 			
 			<hr>
 			
-			<small>&copy;<!-- YEAR --> - <!-- YOUR NAME --></small>
+			<small>&copy;<?php echo date('Y')?> - <?php echo "Ariana"?></small>
 		</div><!-- end wrapper -->
 		
 		<div class="copyright-info">
