3dc79db4f3d2b7135bf78aa4ad0680279c3e7f1d
Ariana <ariaguirre64@gmail.com>
2023-07-04 16:49:34 -0300

    modified

--- 18_AssignmentOperators/practice.php
+++ 18_AssignmentOperators/practice.php
@@ -1,17 +1,13 @@
 <?php
-	
-	// Constants
-
-	
-	// Custom Variables
-
+	define("TITLE", "Assignment Operators");
+	$lessonNum	= 18;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP <!-- TITLE --></title>
+		<title>PHP <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -20,41 +16,65 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 			
-			<h1>Tutorial <!-- LESSON NUMBER -->: <small><!-- TITLE --></small></h1>
+			<h1>Tutorial <?php echo $lessonNum; ?>: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 			
 			<h2>Your Example</h2>
 			
 			<div class="sandbox">
 				
-				<h3>Addition Assignment Operator <code>+=</code></h3>
+			<h3>Addition Assignment Operator <code>+=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a += $b;
+					
+					echo $a;
 				?>
 				
 				<h3>Subtraction Assignment Operator <code>-=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a -= $b;
+					
+					echo $a;
 				?>
 				
 				<h3>Multiplication Assignment Operator <code>*=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a *= $b;
+					
+					echo $a;
 				?>
 				
 				<h3>Division Assignment Operator <code>/=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a /= $b;
+					
+					echo $a;
 				?>
 				
 				<h3>Modulus Assignment Operator <code>%=</code></h3>
 				<?php
-					// your code here
+					$a = 30;
+					$b = 50;
+					$a %= $b;
+					
+					echo $a;
 				?>
 				
 				<h3>Concatenation Assignment Operator <code>.=</code></h3>
 				<?php
-					// your code here
+					$a = "Hola! ";
+					$b = "Esto esta concatenado. ";
+					$a .= $b;
+					
+					echo $a;
 				?>
 				
 			</div><!-- end sandbox -->
@@ -62,12 +82,11 @@
 			<a href="index.php" class="button">Back to the lecture</a>
 			
 			<hr>
-			
-			<small>&copy;<!-- YEAR --> - <!-- NAME --></small>
-		</div><!-- end wrapper -->
+
+		</div>
 		
 		<div class="copyright-info">
 			<?php include('../assets/includes/copyright.php'); ?>
-		</div><!-- end copyright-info -->
+		</div>
 	</body>
 </html>
